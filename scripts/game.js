627a2f386da5725c1157f7ccc61e010072da18f7
rafaelNandes <rafael.fernandessilva@hotmail.com>
2021-05-19 15:48:32 -0300

    checking moves and informing the winner in an alert function

--- scripts/game.js
+++ scripts/game.js
@@ -2,18 +2,54 @@
 let board = ['','','','','','','','',''];
 let playerTime = 0;
 let symbols = ['shield','sword'];
+let gameOver = false;
+let winStates = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [2,4,6],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8]
+];
 
 /*Recebe a posição clicada tratada no arquivo interface.js,
  e atualiza o board com o simbolo do jogadore atual (0 e 1)*/
 function handleMove(position){
     
+    //Quando gameOver for true, a função retornará apenas true e não vai inserir os simbolos e trocar de jogador
+    
+    if(gameOver){
+        return;     
+    }   
+
+    
     if(board[position] == ''){
         board[position] = symbols[playerTime];
 
-        if(playerTime == 0){
-                playerTime = 1;
-        }else{
-            playerTime = 0;
+        gameOver = isWin();
+
+        if(!gameOver){
+            playerTime = (playerTime == 0) ? 1 : 0;          
+        }
+    }
+    
+
+    return gameOver;
+}
+
+function isWin(){
+
+    for(i = 0 ; i < winStates.length ; i++){
+        let sequence = winStates[i];
+
+        let positionA = sequence[0];
+        let positionB = sequence[1];
+        let positionC = sequence[2];
+
+        if(board[positionA] == board[positionB] && board[positionA] == board[positionC] && board[positionA] != ''){
+            return true;
         }
     }
 }
--- scripts/interface.js
+++ scripts/interface.js
@@ -10,30 +10,26 @@ document.addEventListener('DOMContentLoaded', () => {   //Chamar a função apó
 })
 
 function handleClick(event){
-    
     /*
     Chama a função handleMove no arquivo game.js
     Parâmetro é o id da DIV em que houve o evento de clique.
     */
-    handleMove(event.target.id); 
-    
-    updateSquares();
+    let position = event.target.id
+    if(handleMove(position)){
+        
+        setTimeout(() => {
+            alert("O jogo acabou, ganhador é o Player " + playerTime);
+        }, 10);
+        
+    }    
+    updateSquare(position);
 
 }
 
 //Varre os squares da tela novamente e, se o square clicado estiver vazio, adiciona nele o simbolo respectivo do atual jogador.
-function updateSquares(){
-    let squares = document.querySelectorAll(".square");
-
-    squares.forEach((square) => {
-        let position = square.id;
-        let symbol = board[position];
-
-        if(symbol != ''){
-            square.innerHTML = `<div class='${symbol}'> </div>`;
-        }
-    })
-
-   
+function updateSquare(position){
+    let square = document.getElementById(position.toString());
+    let symbol = board[position];
+    square.innerHTML = `<div class='${symbol}'></div>`;
 
 }
